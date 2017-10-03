e104d888a86466cd9d4a8c4da19059e4ecbef5e1
shiversamar <shiversamar@yahoo.com>
2017-10-03 12:43:51 -0700

    add functionally to compliance view

--- src/app/compliance-view/compliance-view.component.ts
+++ src/app/compliance-view/compliance-view.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { CompanyService } from '../services/auth/company.service';
+import { ComplianceService } from '../services/auth/compliance/compliance.service';
+import { Compliance } from '../models/compliance';
+import 'rxjs/add/operator/take';
 
 @Component({
   selector: 'app-compliance-view',
@@ -6,10 +11,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./compliance-view.component.css']
 })
 export class ComplianceViewComponent implements OnInit {
+  companies$;
+  // compliance = {};
+  compliance: Compliance;
+  id;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private companyService: CompanyService,
+    private complianceService: ComplianceService ) { 
+    this.companies$ = companyService.getCompanies();
+        this.id = this.route.snapshot.paramMap.get('id');
+        if (this.id) this.complianceService.get(this.id).take(1).subscribe(c => this.compliance = c);
+      }
 
   ngOnInit() {
   }
 
 }
+
+
+
+
+
+
+
